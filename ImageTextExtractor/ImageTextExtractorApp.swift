207ee871d2f385a1a4bc2f5dbcd3ec47f20ff773
Abhishek Kurmi <itsabhi814@gmail.com>
2025-11-29 11:42:33 +0530

    Initial Commit

---
+++ ImageTextExtractor/ImageTextExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTextExtractorApp.swift
+//  ImageTextExtractor
+//
+//  Created by Apple on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageTextExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
